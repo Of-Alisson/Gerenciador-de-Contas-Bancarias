6ec720822e1884a7db2268171e1e8070050e334e
Alisson Lima <alissonlima50138@gmail.com>
2024-06-04 17:59:58 -0300

    Commit Conta

---
+++ Conta.cs
@@ -0,0 +1,57 @@
+//using System;
+
+public abstract class Conta
+{
+    public string Numero { get; set; }
+    public decimal Saldo{ get; protected set; }
+    public Cliente Cliente{ get; set; }
+
+    public Conta(string numero, Cliente cliente)
+    {
+        Numero = numero;
+        Cliente = cliente;
+        Saldo = 0;
+    } 
+
+    public void Depositar(decimal valor)
+    {
+        if(valor > 0)
+        {
+            Saldo +=valor;
+            Console.WriteLine($"\nDepósito de {Saldo}Depositado com Sucesso");
+        }
+        else
+        {
+            Console.WriteLine("Valor Inválido!!!");
+        }
+    }
+
+    public void Sacar(decimal valor)
+    {
+        if(valor > 0 && valor <=Saldo)
+        {
+            Saldo -= valor;
+
+            Console.WriteLine($"\nSaque de {valor} Realizado com sucesso\n Saldo:{Saldo}");
+        }
+        else
+        {
+            Console.WriteLine($"Saldo insuficiente para Saque\nSaldo:{Saldo}");
+        }
+    }
+
+    public void Transferir(decimal valor, Conta contaDestino)
+    {
+        if(valor > 0 && valor <= Saldo)
+        {
+            Sacar(valor);
+            contaDestino.Depositar(Saldo);
+            Console.WriteLine($"Transferência de {valor} realizada com sucesso!!\nSaldo: {Saldo}");
+        }
+        else
+        {
+            Console.WriteLine($"Falha em realizar a transferência, verifique seu saldo, ou entre em contado com uma agência\n Saldo: {Saldo}");
+        }
+    }
+
+}
--- obj/Debug/net8.0/Gerenciador-de-Contas-Banc-rias.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gerenciador-de-Contas-Banc-rias.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gerenciador-de-Contas-Banc-rias")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab84e88929554e0ab3a385dd790905b70adaa183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a31f6dbcd57602dac58a8cf87e1d92f36def196")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gerenciador-de-Contas-Banc-rias")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gerenciador-de-Contas-Banc-rias")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
